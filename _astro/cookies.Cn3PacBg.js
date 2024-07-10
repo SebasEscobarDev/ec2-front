097265768432896b7180596ac759814c2e74add6
SebasEscobarDev <sebas22185@gmail.com>
2024-07-10 16:09:46 -0500

    Update Project Frontend

---
+++ _astro/cookies.Cn3PacBg.js
@@ -0,0 +1,1 @@
+import{b as t}from"./js.cookie.6G8tq96H.js";const i=(o,e)=>{t.set(o,e)},r=o=>t.get(o)||void 0;export{r as g,i as s};
---
+++ _astro/download-android.DQmmjqWi.js
@@ -0,0 +1,1 @@
+const o={src:"/_astro/download-ios.B_0gaHb4.png",width:209,height:69,format:"png"},s={src:"/_astro/download-android.DlJfZR0I.png",width:209,height:69,format:"png"};export{s as a,o as s};
